73516c3f79c60c722037f2e6797a32bf8ce57e05
kasimbazi <sj623041@gmail.com>
2021-12-28 13:14:25 +0300

    deploy

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <header-bar></header-bar>
+    <b-container>
+      <b-row class="mt-5">
+        <router-view />
+      </b-row>
+    </b-container>
+      <b-row>
+      <data-table></data-table>
+    </b-row>
+  </div>
+</template>
+
+<script>
+import HeaderBar from "@/components/HeaderBar.vue";
+import DataTable from "@/components/DataTable.vue";
+
+
+export default {
+  components: {
+    HeaderBar,
+    DataTable,
+  },
+};
+</script>
